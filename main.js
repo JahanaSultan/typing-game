9e8630a0362a7da83a49fc7bbad1ce48e0c9ef59
JahanaSultan <cahana.aliyeva@gmail.com>
2023-04-08 05:51:54 +0400

    typing game

---
+++ main.js
@@ -0,0 +1,57 @@
+let start_button = document.querySelector(".button-82-pushable")
+let start_screen = document.querySelector(".start-screen")
+let game_screen = document.querySelector(".game-screen")
+let end_screen = document.querySelector(".end-screen")
+let text=document.querySelector("p.text")
+let timerDisplay = document.querySelector('.time-box'); 
+let scoreDisplay=document.querySelector(".score")
+let final_score=document.querySelector(".final-score")
+let input=document.querySelector("input")
+let countdown = 60;
+let score=0
+
+
+const startGame = () => {
+    start_screen.classList.add("hide")
+    game_screen.classList.add("show")
+    input.focus()
+    word()
+    startTimer()
+}
+
+
+function startTimer() {
+    let timer = setInterval(function() {
+        countdown--;
+        timerDisplay.textContent = `0${Math.floor(countdown/60)}:${countdown%60 < 10 ? '0'+countdown%60 : countdown%60}`;
+
+        if (countdown <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+}
+
+function endGame() {
+    game_screen.classList.remove("show");
+    final_score.innerHTML=`Your Final Score is: ${score}`
+    end_screen.classList.add("show");
+}
+
+const word=async()=>{
+    const res=await fetch("https://random-word-api.herokuapp.com/word?number=1")
+    const data=await res.json()
+    text.innerHTML=data[0]
+    check()
+}
+
+const check=()=>{
+    let input=document.querySelector("input")
+    if(input.value==text.innerHTML){
+        word()
+        input.value=""
+        score++
+        scoreDisplay.innerHTML=`RESULT : ${score}`
+    }
+}
+
